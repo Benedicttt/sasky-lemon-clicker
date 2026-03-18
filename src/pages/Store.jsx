16224d0d13d4632fb4689819177ffd11e882c159
benedicttt <benedicttt27051987@gmail.com>
2026-03-18 21:44:28 +0200

    lisää hankittavien tuotteiden suodatuksen

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,22 +1,21 @@
 import Header from '../components/Header.jsx'
 import Item from '../components/Item'
-
+import getPurchasableItems from "../utils/getPurchasableItems";
 
 function Store(props) {
-    const items = props.storeitems.map(item => (
+    const items = getPurchasableItems(props.storeitems).map(item => (
         <Item key={item.id}
               item={item}
               handlePurchase={props.handlePurchase}
               disabled={props.stats.balance < item.price} />
     ))
-    
+
     return (
         <div className="container">
             <Header balance={props.stats.balance}>store</Header>
             <div className="scrollbox items">
                 {items}
             </div>
-
         </div>
     )
 }
